f4891fe20b61dcb6efcb3e9f3ad607231b544e32
“JyngJ” <kaijjy@kaist.ac.kr>
2024-12-01 20:49:23 +0900

    apply update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // 프로젝트 루트 디렉토리 설정
   build: {
     rollupOptions: {
       input: {
-        // left: "./left.html",
-        // right: "./right.html",
-        shared: "./shared.html",
+        main: "shared.html", // 기본 진입점을 shared.html로 설정
       },
     },
+    outDir: "dist", // 빌드 출력 디렉토리
   },
-};
+});
